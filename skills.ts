558f86b01317c390c7c1af627de293b98cc54828
Marcus Murumäe <marcus.murumae@xolo.io>
2023-08-19 23:55:24 +0300

    feat: #2 add core monk skills

--- skills.ts
+++ skills.ts
@@ -1,7 +1,8 @@
 import core_warrior_json from "./skills/core_warrior.json" assert { type: "json" };
 import core_ranger_json from "./skills/core_ranger.json" assert { type: "json" };
+import core_monk_json from "./skills/core_monk.json" assert { type: "json" };
 
-export type Profession = "warrior" | "ranger";
+export type Profession = "warrior" | "ranger" | "monk";
 
 interface Skill {
     wiki_url: string,
@@ -11,6 +12,7 @@ interface Skill {
     image_url: string,
     energy?: string,
     recharge?: string,
+    upkeep?: string,
     profession: Profession,
     attribute: string,
     skill_type: string,
@@ -19,5 +21,7 @@ interface Skill {
 
 const coreWarriorSkills: Skill[] = core_warrior_json as Skill[];
 const coreRangerSkills: Skill[] = core_ranger_json as Skill[];
+const coreMonkSkills: Skill[] = core_monk_json as Skill[];
+const allCoreSkills = coreWarriorSkills.concat(coreRangerSkills).concat(coreMonkSkills);
 
-export { coreWarriorSkills, coreRangerSkills };
+export { coreWarriorSkills, coreRangerSkills, coreMonkSkills, allCoreSkills };
