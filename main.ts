576ccceaf5633e0c7cc9c23f048bd30b0de5f67e
Marcus Murumäe <marcus.murumae@xolo.io>
2023-08-19 20:28:11 +0300

    feat/1-add-core-ranger-skills

--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
-import coreWarriorSkills from "./skills.ts";
+import { coreWarriorSkills, coreRangerSkills, Profession } from "./skills.ts";
 
 const router = new Router();
 router
   .get("/", (context) => {
     context.response.body = "Welcome to Guild Wars skills API!";
   })
-  .get("/api", (context) => {
-    context.response.body = coreWarriorSkills;
+  .get("/api/profession/:profession", (context) => {
+    if (context?.params?.profession) {
+      const profession = context.params.profession as Profession;
+      if (profession === "warrior") {
+        context.response.body = coreWarriorSkills;
+      } else if (profession === "ranger") {
+        context.response.body = coreRangerSkills;
+      } else {
+        context.response.body = `No skills found for {profession}`;
+        context.response.status = 400;
+      }
+    }
   })
-  .get("/api/:name", (context) => {
+  .get("/api/name/:name", (context) => {
     if (context?.params?.name) {
-      const found = coreWarriorSkills.find((skill) =>
+      const skills = coreWarriorSkills.concat(coreRangerSkills);
+      const found = skills.find((skill) =>
         skill.name.toLowerCase() === context.params.name.toLowerCase()
       );
       if (found) {
--- skills.ts
+++ skills.ts
@@ -1,4 +1,7 @@
-import coreWarriorSkills from "./skills/core_warrior.json" assert { type: "json" };
+import core_warrior_json from "./skills/core_warrior.json" assert { type: "json" };
+import core_ranger_json from "./skills/core_ranger.json" assert { type: "json" };
+
+export type Profession = "warrior" | "ranger";
 
 interface Skill {
     wiki_url: string,
@@ -8,10 +11,13 @@ interface Skill {
     image_url: string,
     energy?: string,
     recharge?: string,
-    profession: string,
+    profession: Profession,
     attribute: string,
     skill_type: string,
     campaign: string,
 }
 
-export default coreWarriorSkills as Skill[];
+const coreWarriorSkills: Skill[] = core_warrior_json as Skill[];
+const coreRangerSkills: Skill[] = core_ranger_json as Skill[];
+
+export { coreWarriorSkills, coreRangerSkills };
